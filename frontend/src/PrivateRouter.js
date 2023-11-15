4ab089d906cb3789901c0390b933e7427093fe75
Alexandre <alexandre.gerard.dev@gmail.com>
2023-11-15 16:18:48 +0100

    New folder

--- frontend/src/PrivateRouter.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRoute = () => {
-  const token = window.localStorage.getItem("userInfo");
-  return token ? <Outlet /> : <Navigate to={"/login"} />;
-};
-
-export default PrivateRoute;
